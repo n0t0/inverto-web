37e7e63413206d7c0d428d98eb0cf7a7831e9215
Ivo Ivanov <vn.n0t0@gmail.com>
2017-02-11 16:27:05 -0800

    Footer added; *.svg not recognizable

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <p id="logo_43d">
+    <a href="<?php echo esc_url(); ?>" target="_blank" title="Inverto Records"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/inverto_logo_75x75.svg"
+ alt="Inverto Records"/></a>
+  </p>
+</footer>
+
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
